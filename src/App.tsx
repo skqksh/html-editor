b8e3efc9ffb7ed79a9a1b137654bbfd2e6edacb4
blue <skqksh@gmail.com>
2023-05-11 21:13:42 +0900

    Init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ReactQuill from "react-quill";
 
 function App() {
+  const [selectedHtml, setSelectedHtml] = useState("");
+
+  const getFile = (files: FileList) => {
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+      const text = e.target?.result;
+      if (typeof text === "string") {
+        setSelectedHtml(text);
+      }
+    };
+    reader.readAsText(files[0]);
+  };
+
+  const onClickDownload = (): void => {
+    const date = new Date();
+    const filename = `newFile-${date.getFullYear()}-${
+      date.getMonth() + 1
+    }-${date.getDate()}.html`;
+    let element = document.createElement("a");
+    element.setAttribute(
+      "href",
+      "data:text/html;charset=utf-8," + encodeURIComponent(selectedHtml)
+    );
+    element.setAttribute("download", filename);
+    element.style.display = "none";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ padding: 10 }}>
+        <input
+          type="file"
+          onChange={(e): void => {
+            e.target.files && getFile(e.target.files);
+          }}
+          accept=".html"
+        />
+        <button onClick={onClickDownload}>다운로드</button>
+      </div>
+      <hr />
+      <ReactQuill
+        theme="snow"
+        value={selectedHtml}
+        onChange={setSelectedHtml}
+      />
     </div>
   );
 }
